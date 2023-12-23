d00814ee567bec2fb6e17f7cf124d95ea1a73dc5
sadok-dorbez <sadokdorbez0@gmail.com>
2023-12-23 17:39:10 +0100

    adding global.css

--- src/app/components/MaxWidthWrapper.tsx
+++ src/app/components/MaxWidthWrapper.tsx
@@ -1,3 +1,4 @@
+import { cn } from "@/lib/utils"
 import { ReactNode } from "react"
 
 const MaxWidthWrapper = ({
@@ -8,7 +9,7 @@ const MaxWidthWrapper = ({
     children: ReactNode
 }) => {
     return(
-    <div className='mx-auto w-full max-w-screen-xl px-2.5 md:px-20'>
+    <div className= {cn('mx-auto w-full max-w-screen-xl px-2.5 md:px-20', className)}>
         {children}
     </div>
     )
